039fb9b9c722f53100c3046b126cac9770038588
devrilynn <130400488+devrilynn@users.noreply.github.com>
2023-05-10 16:50:37 -0700

    Update DDL.sql

--- sql/DDL.sql
+++ sql/DDL.sql
@@ -1,88 +1,126 @@
 SET FOREIGN_KEY_CHECKS=0;
-SET AUTOCOMMIT = 0
+SET AUTOCOMMIT = 0;
 
+--
+-- Table structure for table `Employees`
+--
 
 CREATE OR REPLACE TABLE Employees (
     employee_id int not null auto_increment,
-    first_name nvarchar(255) not null,
-    last_name nvarchar(255) not null,
-    email nvarchar(255) not null,
-    department_id int not null,
+    first_name varchar(255) not null,
+    last_name varchar(255) not null,
+    email varchar(255) not null,
+    dept_id int not null,
     active bit not null,
     hire_date date,
     role_id int,
     primary key(employee_id),
     foreign key(role_id) references Roles(role_id),
-    foreign key(department_id) references Departments(department_id),
+    foreign key(dept_id) references Departments(dept_id),
     on delete set null  -- if departemnt or role are deleted we dont necessarily want to delete the employee
 );
 
-INSERT INTO Employees(employee_id, first_name, last_name, email, department, active, hire_date, role)
+--
+-- Dumping data for table `Employees`
+--
+
+INSERT INTO Employees(employee_id, first_name, last_name, email, dept_id, active, hire_date, role_id)
 VALUES 
-(1, ‘Devri’, ‘Anderson’, ‘da@coder.com’, 1, 1, ‘2023-1-1’, 1),
-(2, ‘Jacob’, ‘Ogle’, ‘jo@coder.com’, 3, 1,’2023-1-2’,  2),
+(1, ‘Devri’, ‘Anderson’, ‘da@coder.com’, 1, 1, ‘2023-01-01’, 1),
+(2, ‘Jacob’, ‘Ogle’, ‘jo@coder.com’, 3, 1,’2023-01-02’,  2),
 (3, ‘Michael’, ‘Scott’, ‘prisonmike@coder.com’, 2, 1, ‘2023-04-29’, 9);
 
+--
+-- Table structure for table `Devices`
+--
 
 CREATE OR REPLACE TABLE Devices(
     device_id int not null auto_increment,
-    device_name nvarchar(255) not null,
-    type nvarchar(255),
+    device_name varchar(255) not null,
+    type varchar(255),
     access_level int not null,
-    usb_access int not null,
+    usb_access bit not null,
     primary key(device_id),
     employee_id int,
     foreign key(employee_id) references Employees(employee_id),
     on delete set null -- if employee is deleted we may want to keep device
 );
 
-INSERT INTO Devices(device_id, device_name, type, access_level, usb_access)
+--
+-- Dumping data for table `Devices`
+--
+
+INSERT INTO Devices(device_id, device_name, type, access_level, usb_access, employee_id)
 VALUES
 (1, ‘Macbook Pro’, ‘Laptop’, 3, 0, 3),
 (2, ‘iPhone 14’, ’Mobile’, 1, 1, 2),
 (3, ‘iMac’, ‘Desktop’, 1, 1, 1);
 
+--
+-- Table structure for table `Departments`
+--
+
 CREATE OR REPLACE TABLE Departments(
-    dept_id int auto_increment,
-    name - nvarchar(255),
-    manager - emp_id,
+    dept_id int not null auto_increment,
+    dept_name nvarchar(255),
+    manager_employee_id int,
     primary key(dept_id),
-    foreign key(manager) references Employees(employee_id),
+    foreign key(manager_employee_id) references Employees(employee_id),
     on delete set null -- if employee is deleted we mw want to keep the dept
 );
 
-INSERT INTO Departments(dept_id, name, manager)
+--
+-- Dumping data for table `Departments`
+--
+
+INSERT INTO Departments(dept_id, dept_name, manager_employee_id)
 VALUES
 (1, ‘Software Engineering’, 1),
 (2, ‘Management’, 3),
 (3, ‘Data Analytics’, 2);
 
+--
+-- Table structure for table `Roles`
+--
+
 CREATE OR REPLACE TABLE Roles(
-    role_id int auto_increment,
+    role_id int not null auto_increment,
     title nvarchar(255),
-    access_level int ,
+    access_level int,
     primary key(role_id)
 );
+
+--
+-- Dumping data for table `Roles`
+--
 
 INSERT INTO Roles(role_id, title, access_level)
 VALUES
 (1, ‘Software Engineer’, 1),
 (2, ‘Data Engineer’, 1),
-(3, ‘Intern’, 10)
+(3, ‘Intern’, 10),
 (4, ‘CEO’, 1),
 (5, ‘CTO’, 1),
-(6, ‘CFO’, 1)
+(6, ‘CFO’, 1),
 (7, ‘IT’, 1),
 (8, ‘Assistant to General Manager’, 6),
 (9, ‘General Manager’, 3);
 
+--
+-- Table structure for table `Trainings`
+--
+
 CREATE OR REPLACE TABLE Trainings(
-    training_id int auto_increment,
+    training_id int not null auto_increment,
     title nvarchar(255),
     duration_in_min int,
     required_status bit not null,
     primary key(training_id),
 );
+
+--
+-- Dumping data for table `Trainings`
+--
 
 INSERT INTO Trainings(training_id, title, duration_in_min, required_status)
 VALUES
@@ -90,37 +128,54 @@ VALUES
 (2, ‘Reverse Running in Red Rock’, 10, 0),
 (3, ‘Don’t get hacked 101’, 60, 0);
 
+--
+-- Table structure for table `Passwords`
+--
+
 CREATE OR REPLACE TABLE Passwords(
-    password_id int auto_increment,
-    pass nvarchar(255),
+    password_id int not null auto_increment,
+    password varchar(255) not null,
     req_change bit not null,
+    employee_id int,
     primary key(password_id),
     foreign key(employee_id) references Employees(employee_id),
-    cascade on delete -- if employee is deleted no reason to keep password
+    on delete cascade -- if employee is deleted no reason to keep password
 );
 
-INSERT INTO Passwords(password_id, title, password, req_change, employee_id)
+--
+-- Dumping data for table `Passwords`
+--
+
+INSERT INTO Passwords(password_id, password, req_change, employee_id)
 VALUES
 (1, ‘abc123*!’, 0, 1),
 (2, ‘password’, 0, 2),
-(3, ‘DunderMifflin1’, 1, 0);
+(3, ‘DunderMifflin1’, 1, 3);
 
+--
+-- Table structure for table `TrainingDetails`
+--
 
 CREATE OR REPLACE TABLE TrainingDetails(
-    training_details_id int auto_increment,
+    training_details_id int not null auto_increment,
+    employee_id int,
     training_id int,
     completion_date date,
     pass_or_fail bit not null,
     foreign key(employee_id) references Employees(employee_id),
     foreign key(training_id) references Trainings(training_id),
-    cascade on delete -- if the employee or training is deleted there is no reason to keep training record
+    on delete cascade -- if the employee is deleted there is no reason to keep training record
 );
 
-INSERT INTO TrainingDetails(training_id, title, duration_in_min, required_status)
+--
+-- Dumping data for table `TrainingDetails`
+--
+
+INSERT INTO TrainingDetails(training_details_id, employee_id, training_id, completion_date, pass_or_fail)
 VALUES
-(1, 2, 1, ‘2023-1-5’, 1),
-(2, 3, 3, ‘2023-5-1’, 1),
-(3, 1, 2, ‘2023-6-1’, 1);
+(1, 2, 1, ‘2023-05-01’, 1),
+(2, 3, 3, ‘2023-01-05’, 1),
+(3, 1, 2, ‘2023-06-01’, 1);
 
 SET FOREIGN_KEY_CHECKS=1;
 COMMIT;
