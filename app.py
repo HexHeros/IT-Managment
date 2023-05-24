ee6f877cf621049655591f947e0c81b5d98fe3ad
Jacob Ogle <123908271+JacobOgle@users.noreply.github.com>
2023-05-23 23:17:33 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from unittest import result
-from flask import Flask, render_template, json, request
+from flask import Flask, render_template, json, request, redirect, url_for
 import os
 import database.db_connector as db
 import sys
@@ -17,11 +17,12 @@ except RuntimeError:
 def root():
     return render_template("home.html")
 
-@app.route('/employees')
+@app.route('/employees', methods=["GET"])
 def employees():
-    query = "select * from Employees;"
-    cursor = db.execute_query(db_connection=db_connection, query=query)
-    results = cursor.fetchall()
+    if request.method == "GET":
+        query = "select * from Employees;"
+        cursor = db.execute_query(db_connection=db_connection, query=query)
+        results = cursor.fetchall()
     return render_template("employees.html", employees=results)
 
 @app.route('/departments')
@@ -51,28 +52,32 @@ def new_employee():
         query = "INSERT INTO Employees( first_name, last_name, email, dept_id, active, hire_date, role_id )\n"
         vals = f"values ('{fn}', '{ln}', '{email}', {dept_id}, {active}, '{hire_date}', {role_id})" 
         cursor = db.execute_query(db_connection=db_connection, query=(query+vals))
-        return render_template("home.html")
+        return redirect(url_for('employees'))
     return render_template("new_employee.html")
 
 @app.route('/edit_employee/<int:id>', methods=["GET", "POST"])
 def edit_employee(id):
     if request.method == "POST":
+        eid = request.form['employee_id']
         fn = request.form['first_name']
         ln = request.form['last_name']
         email = request.form['email']
         dept_id = int(request.form['dept_id'])
         active = 1 if request.form["active"] == 'on' else 0
-        hire_date = request.form["hire_date"]
         role_id = int(request.form["role_id"])
-        query = f"UPDATE Employees SET first_name = '{fn}', last_name = '{ln}', email = '{email}', dept_id = {dept_id}, active = {active}, hire_date = '{hire_date}', role_id = {role_id} WHERE employee_id = {id}"
+        query = f"UPDATE Employees SET first_name = '{fn}', last_name = '{ln}', email = '{email}', dept_id = {dept_id}, active = {active}, role_id = {role_id} WHERE employee_id = {eid}"
         cursor = db.execute_query(db_connection=db_connection, query=query)
-        
-        return render_template('employees.html')
-    query = f"SELECT * FROM Employees WHERE employee_id = {id}"
-    cursor = db.execute_query(db_connection=db_connection, query=query)
-    result = cursor.fetchone()
-    
-    return render_template("edit_employee.html", employee=result)
+        return redirect(url_for('employees'))
+    if request.method == "GET":
+        query = f"select * from Employees where employee_id={id};"
+        cursor = db.execute_query(db_connection=db_connection, query=query)
+        results = cursor.fetchall()
+        return render_template("edit_employee.html", employees=results)
+    else:
+        query = f"SELECT * FROM Employees WHERE employee_id = {id}"
+        cursor = db.execute_query(db_connection=db_connection, query=query)
+        result = cursor.fetchall()
+        return render_template("edit_employee.html", employees=result)
 
 @app.route('/roles')
 def roles():
