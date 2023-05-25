dfc321ba8af9d0ffd709b74672064b74f8b1f8ca
Jacob Ogle <123908271+JacobOgle@users.noreply.github.com>
2023-05-25 19:02:21 -0400

    Please work

--- app.py
+++ app.py
@@ -1,16 +1,17 @@
-from unittest import result
 from flask import Flask, render_template, json, request, redirect, url_for
+from flask_mysqldb import MySQL
 import os
-import database.db_connector as db
-import sys
-# Configuration
 
+# Configuration
 app = Flask(__name__)
-try:
-    db_connection = db.connect_to_database()
-except RuntimeError:
-    print("Error connecting to the database")
-    sys.exit(-1)
+app.config['DEBUG']             = True
+app.config['MYSQL_HOST']        = 'classmysql.engr.oregonstate.edu'
+app.config['MYSQL_USER']        = 'cs340_ogleja'
+app.config['MYSQL_PASSWORD']    = '9706' #last 4 of onid
+app.config['MYSQL_DB']          = 'cs340_ogleja'
+app.config['MYSQL_CURSORCLASS'] = "DictCursor"
+
+mysql = MySQL(app)
 
 # Routes 
 @app.route('/')
@@ -19,10 +20,11 @@ def root():
 
 @app.route('/employees', methods=["GET"])
 def employees():
+    cur = mysql.connection.cursor()
     if request.method == "GET":
         query = "select * from Employees;"
-        cursor = db.execute_query(db_connection=db_connection, query=query)
-        results = cursor.fetchall()
+        cur.execute(query)
+        results = cur.fetchall()
     return render_template("employees.html", employees=results)
 
 @app.route('/departments')
@@ -41,50 +43,56 @@ def devices():
 
 @app.route('/new_employee', methods=["GET", "POST"])
 def new_employee():
+    cur = mysql.connection.cursor()
     if request.method == "POST":
         fn = request.form['first_name']
         ln = request.form['last_name']
         email = request.form['email']
         dept_id = int(request.form['dept_id'])
-        active = 1 if request.form["active"] == 'on' else 0
+        active = request.form["active"]
         hire_date = request.form["hire_date"]
         role_id = int(request.form["role_id"])
         query = "INSERT INTO Employees( first_name, last_name, email, dept_id, active, hire_date, role_id )\n"
         vals = f"values ('{fn}', '{ln}', '{email}', {dept_id}, {active}, '{hire_date}', {role_id})"
-        cursor = db.execute_query(db_connection=db_connection, query=(query+vals))
+        cur.execute(query+vals)
+        mysql.connection.commit()
         return redirect(url_for('employees'))
     return render_template("new_employee.html")
 
 @app.route('/edit_employee/<int:id>', methods=["GET", "POST"])
 def edit_employee(id):
+    cur = mysql.connection.cursor()
     if request.method == "POST":
         eid = request.form['employee_id']
         fn = request.form['first_name']
         ln = request.form['last_name']
         email = request.form['email']
         dept_id = int(request.form['dept_id'])
-        active = 1 if request.form["active"] == 'on' else 0
+        active = request.form["active"]
         hire_date = request.form["hire_date"]
         role_id = int(request.form["role_id"])
         query = f"UPDATE Employees SET first_name = '{fn}', last_name = '{ln}', email = '{email}', dept_id = {dept_id}, active = {active}, hire_date = '{hire_date}', role_id = {role_id} WHERE employee_id = {eid}"
-        cursor = db.execute_query(db_connection=db_connection, query=query)
+        cur.execute(query)
+        mysql.connection.commit()
         return redirect(url_for('employees'))
     if request.method == "GET":
         query = f"select * from Employees where employee_id={id};"
-        cursor = db.execute_query(db_connection=db_connection, query=query)
-        results = cursor.fetchall()
+        cur.execute(query)
+        results = cur.fetchall()
         return render_template("edit_employee.html", employees=results)
     else:
-        query = f"SELECT * FROM Employees WHERE employee_id = {id}"
-        cursor = db.execute_query(db_connection=db_connection, query=query)
-        result = cursor.fetchall()
+        query = f"SELECT * FROM Employees WHERE employee_id = %s"
+        cur.execute(query, (id,))
+        result = cur.fetchall()
         return render_template("edit_employee.html", employees=result)
 
 @app.route("/delete_employee/<int:id>")
 def delete_people(id):
+    cur = mysql.connection.cursor()
     # mySQL query to delete the person with our passed id
-    query = f"DELETE FROM Employees WHERE employee_id = {id};"
-    cursor = db.execute_query(db_connection=db_connection, query=query)
+    query = f"DELETE FROM Employees WHERE employee_id = %s;"
+    cur.execute(query, (id,))
+    mysql.connection.commit()
     # redirect back to people page
     return redirect(url_for('employees'))
 
@@ -110,7 +118,6 @@ def passwords():
     return render_template("passwords.html")
 
 # Listener
-
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 9113))
     app.run(port=port, debug=True)
