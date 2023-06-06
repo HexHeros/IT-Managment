5825be3881b345f6fc90e5a64f2b78ddd7e8481c
devrilynn <130400488+devrilynn@users.noreply.github.com>
2023-06-06 07:42:02 -0700

    Update app.py
    updated devices with JOIN

--- app.py
+++ app.py
@@ -226,7 +226,7 @@ def devices():
     cur = mysql.connection.cursor()
     if request.method == "GET":
         # Retrieve all departments in the database
-        query = "SELECT * from Devices"
+        query = "SELECT d.device_name, d.type, d.access_level, d.usb_access, e.first_name, e.last_name from Devices d JOIN Employees e ON d.employee_id = e.employee_id;"
         cur.execute(query)
         devices = cur.fetchall()
     return render_template("devices.html", devices=devices)
@@ -249,7 +249,11 @@ def new_device():
         mysql.connection.commit()
         return redirect(url_for('devices'))
     
-    return render_template("new_device.html")
+    query = f"SELECT employee_id, first_name, last_name FROM Employees;"
+    cur.execute(query)
+    employees= cur.fetchall()
+    
+    return render_template("new_device.html", employees=employees)
 
 @app.route("/delete_device/<int:id>")
 def delete_device(id):
